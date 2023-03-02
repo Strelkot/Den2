0abfcb570ba59e26d93f1ff23492ff3cf934d1e0
Strelkot <126769945+Strelkot@users.noreply.github.com>
2023-03-02 15:17:23 +0500

    Add files via upload

---
+++ Debil2/Debil2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Debil2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int i = 3;
+            double y = 4.12;
+            decimal d = 600;
+            string s = "Hello";
+            Console.Write(" i = "); Console.WriteLine(i);
+            Console.Write(" y = "); Console.WriteLine(y);
+            Console.Write(" d = "); Console.WriteLine(d);
+            Console.Write("s = "); Console.WriteLine(s);
+        }
+    }
+}
